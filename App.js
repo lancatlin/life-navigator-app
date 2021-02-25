8a5711c84f9e1e566fbf21c860a04f5b2e1bb094
wancat <wancat@wancat.cc>
2021-02-25 22:33:08 +0800

    Basic Navigation Setup

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AuthScreen from './src/screens/AuthScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Sign in" component={AuthScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
