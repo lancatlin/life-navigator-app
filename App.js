9ea68e8e995e101df7aa28241701243dd3a1a567
Lazy <machael1209@gmail.com>
2021-03-06 16:44:41 +0800

    HomeScreen, GoalScreen

--- App.js
+++ App.js
@@ -6,15 +6,25 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AuthScreen from './src/screens/AuthScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import GoalsScreen from './src/screens/GoalsScreen';
+import GoalDetailScreen from './src/screens/GoalDetailScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const goalsFlow = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Goals List" component={GoalsScreen} options={{ headerTitleAlign: 'center' }} />
+    <Stack.Screen name="Detail" component={GoalDetailScreen} options={{ headerTitleAlign: 'center' }} />
+  </Stack.Navigator>
+);
 
 export default () => (
   <NavigationContainer>
     <Tab.Navigator>
       <Tab.Screen name="Sign in" component={AuthScreen} />
       <Tab.Screen name="Home" component={HomeScreen} />
-    </Tab.Navigator>
+      <Tab.Screen name="Goals" component={goalsFlow} />
+    </Tab.Navigator> 
   </NavigationContainer>
 );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,63 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+const fakeData = [
+  {title: 'Calculus'},
+  {title: 'Codewars'},
+  {title: 'Exercise'},
+  {title: 'Walk the dog'},
+  {title: 'Physics'}
+]
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <View style={styles.viewStyle}>
+      <Text style={styles.topTextStyle}>Upcoming Events</Text>
+      <AntDesign
+        name="clockcircleo"
+        size={250}
+        style={styles.clockStyle}
+      />
+      <FlatList //NOT SCROLLABLE!!!
+        style={styles.listStyle}
+        data={fakeData}
+        keyExtractor={item => item.title}
+        renderItem={({ item }) => <Text style={styles.textStyle}>{item.title}</Text>}
+      />
+      </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    marginHorizontal: 20
+  },
+  topTextStyle: {
+    fontSize: 35,
+    marginTop: 55,
+    marginBottom: 25,
+    alignSelf: 'center'
+  },
+  clockStyle: {
+    alignSelf: 'center'
+  },
+  listStyle: {
+    marginVertical: 50
+
+  },
+  textStyle: {
+    backgroundColor: 'rgba(145, 190, 240, 0.3)',
+    borderColor: 'rgba(145, 190, 240, 1.0)',
+    borderWidth: 2,
+    borderRadius: 30,
+    fontSize: 25,
+    height: 60,
+    margin: 30,
+    marginVertical: 5,
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  }
+});
 
 export default HomeScreen;
