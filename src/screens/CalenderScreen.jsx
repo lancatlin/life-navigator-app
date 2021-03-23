95d694dc03e2e460c7fecb53671a5b17526d8636
shoko <michael52848@gmail.com>
2021-03-24 02:19:50 +0800

    Calender繪圖更新

--- src/screens/CalenderScreen.jsx
+++ src/screens/CalenderScreen.jsx
@@ -3,42 +3,59 @@ import {
   Text, View, StyleSheet, FlatList, 
 } from 'react-native';
 
+const fakedata = [
+  {
+    date: 'Tue',
+    day: '2',
+    schedule: ['Calculus', 'Codewars', 'Trigonmetry'],
+  },
+  {
+    date: 'Wed',
+    day: '3',
+    schedule: ['Calculus', 'Codewars', 'Trigonmetry'],
+  },
+  {
+    date: 'Thu',
+    day: '4',
+    schedule: ['Calculus', 'Codewars', 'Trigonmetry'],
+    time: ['9:00 - 11: 00', '13:00 - 15:00', '15:30 - 17:30']
+  }
+]
 const CalenderScreen = () => (
-  <View style={styles.drawer}>
-    <View style={styles.dateDrawer}>
-      <Text style={styles.day}>Tue</Text>
-      <Text style={styles.date}>2</Text>
-    </View>
-    <View style={styles.scheduleDrawer}>
-     <FlatList
-        data={[
-          {key: ['Calculus', '9:00-11:00']},
-          {key: ['Codewars', '13:00-15:00']},
-          {key: ['Trigonmetry', '15:30-17:30']}
-        ]}
-        renderItem={({item}) => 
-          <View style={styles.schedule}>
-            <View style={styles.scheduleView}>
-              <Text style={styles.Text}>{item.key[0]}</Text>
+  <FlatList
+    data={fakedata}
+    keyExtractor={(item) => item.date}
+    renderItem={({item}) => 
+      <View style={styles.drawer}>
+        <View style={styles.dateDrawer}>
+          <Text style={styles.day}>{item.date}</Text>
+          <Text style={styles.date}>{item.day}</Text>
+        </View>
+        <FlatList
+          data={item.schedule}
+          keyExtractor={(item) => item}
+          renderItem={({item}) => 
+            <View style={styles.scheduleDrawer}>
+              <Text style={styles.Text}>{item}</Text>
+              <View style={styles.timeView}>
+                <Text style={styles.Text}>time</Text>
+              </View>              
             </View>
-            <View style={styles.timeView}>
-              <Text style={styles.Text}> {item.key[1]} </Text>
-            </View>
-          </View>
-        }
-      />
-    </View>
-  </View>
+          }
+        />
+      </View>
+    }
+  />
 );
 
 const styles = StyleSheet.create({
   drawer:{
-    flexDirection: 'row',
-    flex: 1
+    flexDirection: 'row'
   },
   dateDrawer:{
-    left: 30,
-    alignItems: 'center'
+    left: 20,
+    alignItems: 'center',
+    width: '10%'
   },
   date:{
     fontSize: 20
@@ -47,22 +64,20 @@ const styles = StyleSheet.create({
     fontSize: 17
   },
   scheduleDrawer:{
-    left: 45,
-    flex: 0.85
-  },
-  schedule:{
+    left: 35,
     backgroundColor: 'pink',
     borderColor: 'red',
     borderWidth: 1,
     borderRadius: 15,
-    flex: 1,
     margin: 10,
     height: 45, 
     alignItems: 'center',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    width: 300
   },
   Text:{
     fontSize: 17,
+    marginHorizontal: 5,
   },
   timeView:{
     alignItems: 'flex-end',
