c186ab7a25699177e2c122b25c73647f8c954e08
wancat <wancat@pm.me>
2021-03-26 19:56:10 +0800

    Fix style

--- src/screens/CalenderScreen.jsx
+++ src/screens/CalenderScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Text, View, StyleSheet, FlatList, 
+  Text, View, StyleSheet, FlatList,
 } from 'react-native';
 
 const fakedata = [
@@ -18,14 +18,14 @@ const fakedata = [
     date: 'Thu',
     day: '4',
     schedule: ['Calculus', 'Codewars', 'Trigonmetry'],
-    time: ['9:00 - 11: 00', '13:00 - 15:00', '15:30 - 17:30']
-  }
-]
+    time: ['9:00 - 11: 00', '13:00 - 15:00', '15:30 - 17:30'],
+  },
+];
 const CalenderScreen = () => (
   <FlatList
     data={fakedata}
     keyExtractor={(item) => item.date}
-    renderItem={({item}) => 
+    renderItem={({ item }) => (
       <View style={styles.drawer}>
         <View style={styles.dateDrawer}>
           <Text style={styles.day}>{item.date}</Text>
@@ -34,55 +34,55 @@ const CalenderScreen = () => (
         <FlatList
           data={item.schedule}
           keyExtractor={(item) => item}
-          renderItem={({item}) => 
+          renderItem={({ item }) => (
             <View style={styles.scheduleDrawer}>
               <Text style={styles.Text}>{item}</Text>
               <View style={styles.timeView}>
                 <Text style={styles.Text}>time</Text>
-              </View>              
+              </View>
             </View>
-          }
+          )}
         />
       </View>
-    }
+    )}
   />
 );
 
 const styles = StyleSheet.create({
-  drawer:{
-    flexDirection: 'row'
+  drawer: {
+    flexDirection: 'row',
   },
-  dateDrawer:{
+  dateDrawer: {
     left: 20,
     alignItems: 'center',
-    width: '10%'
+    width: '10%',
   },
-  date:{
-    fontSize: 20
+  date: {
+    fontSize: 20,
   },
-  day:{
-    fontSize: 17
+  day: {
+    fontSize: 17,
   },
-  scheduleDrawer:{
+  scheduleDrawer: {
     left: 35,
     backgroundColor: 'pink',
     borderColor: 'red',
     borderWidth: 1,
     borderRadius: 15,
     margin: 10,
-    height: 45, 
+    height: 45,
     alignItems: 'center',
     flexDirection: 'row',
-    width: 300
+    width: 300,
   },
-  Text:{
+  Text: {
     fontSize: 17,
     marginHorizontal: 5,
   },
-  timeView:{
+  timeView: {
     alignItems: 'flex-end',
-    flex: 1
-  }
+    flex: 1,
+  },
 });
 
 export default CalenderScreen;
