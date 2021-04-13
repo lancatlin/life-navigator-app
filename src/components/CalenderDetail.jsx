70e6a37ca2ab3d7d1a84ee147390a027e9f3c106
wancat <wancat@pm.me>
2021-04-13 21:04:00 +0800

    Format code

--- src/components/CalenderDetail.jsx
+++ src/components/CalenderDetail.jsx
@@ -1,104 +1,106 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Modal, StyleSheet } from 'react-native';
+import {
+  View, Text, Modal, StyleSheet,
+} from 'react-native';
 
 const fakedata = [
-    {
-      id: 1,
-      name: 'Calculus',
-      goalId: 3,
-      startTime: new Date(2021, 1, 2, 9, 0),
-      endTime: new Date(2021, 1, 2, 11, 0),
-    },
-    {
-      id: 2,
-      name: 'Codewars',
-      goalId: 2,
-      startTime: new Date(2021, 1, 2, 13, 0),
-      endTime: new Date(2021, 1, 2, 15, 0)
-    },
-    {
-      id: 3,
-      name: 'Trigonmetry',
-      goalId: 3,
-      startTime: new Date(2021, 1, 2, 15, 30),
-      endTime: new Date(2021, 1, 2, 17, 30)
-    },
-    {
-      id: 4,
-      name: 'Calculus',
-      goalId: 3,
-      startTime: new Date(2021, 1, 3, 9, 0),
-      endTime: new Date(2021, 1, 3, 11, 0),
-    },
-    {
-      id: 5,
-      name: 'Codewars',
-      goalId: 2,
-      startTime: new Date(2021, 1, 3, 13, 0),
-      endTime: new Date(2021, 1, 3, 15, 0)
-    },
-    {
-      id: 6,
-      name: 'Trigonmetry',
-      goalId: 3,
-      startTime: new Date(2021, 1, 3, 15, 30),
-      endTime: new Date(2021, 1, 3, 17, 30)
-    },
-    {
-      id: 7,
-      name: 'Calculus',
-      goalId: 3,
-      startTime: new Date(2021, 1, 4, 9, 0),
-      endTime: new Date(2021, 1, 4, 11, 0),
-    },
-    {
-      id: 8,
-      name: 'Codewars',
-      goalId: 2,
-      startTime: new Date(2021, 1, 4, 13, 0),
-      endTime: new Date(2021, 1, 4, 15, 0)
-    },
-    {
-      id: 9,
-      name: 'Trigonmetry',
-      goalId: 3,
-      startTime: new Date(2021, 1, 4, 15, 30),
-      endTime: new Date(2021, 1, 4, 17, 30)
-    },
+  {
+    id: 1,
+    name: 'Calculus',
+    goalId: 3,
+    startTime: new Date(2021, 1, 2, 9, 0),
+    endTime: new Date(2021, 1, 2, 11, 0),
+  },
+  {
+    id: 2,
+    name: 'Codewars',
+    goalId: 2,
+    startTime: new Date(2021, 1, 2, 13, 0),
+    endTime: new Date(2021, 1, 2, 15, 0),
+  },
+  {
+    id: 3,
+    name: 'Trigonmetry',
+    goalId: 3,
+    startTime: new Date(2021, 1, 2, 15, 30),
+    endTime: new Date(2021, 1, 2, 17, 30),
+  },
+  {
+    id: 4,
+    name: 'Calculus',
+    goalId: 3,
+    startTime: new Date(2021, 1, 3, 9, 0),
+    endTime: new Date(2021, 1, 3, 11, 0),
+  },
+  {
+    id: 5,
+    name: 'Codewars',
+    goalId: 2,
+    startTime: new Date(2021, 1, 3, 13, 0),
+    endTime: new Date(2021, 1, 3, 15, 0),
+  },
+  {
+    id: 6,
+    name: 'Trigonmetry',
+    goalId: 3,
+    startTime: new Date(2021, 1, 3, 15, 30),
+    endTime: new Date(2021, 1, 3, 17, 30),
+  },
+  {
+    id: 7,
+    name: 'Calculus',
+    goalId: 3,
+    startTime: new Date(2021, 1, 4, 9, 0),
+    endTime: new Date(2021, 1, 4, 11, 0),
+  },
+  {
+    id: 8,
+    name: 'Codewars',
+    goalId: 2,
+    startTime: new Date(2021, 1, 4, 13, 0),
+    endTime: new Date(2021, 1, 4, 15, 0),
+  },
+  {
+    id: 9,
+    name: 'Trigonmetry',
+    goalId: 3,
+    startTime: new Date(2021, 1, 4, 15, 30),
+    endTime: new Date(2021, 1, 4, 17, 30),
+  },
 ];
 
 function ModalOpen(id) {
-    const [open, setOpen] = useState(false)
-    useEffect(() => {
-      for( var i = 0; i < fakedata.length; i++){
-        if ( fakedata[i].id == id ) {
-          setOpen(true)
-          return(
-            <Modal
-              visible={open}
-              transparent
-              onRequestClose={setOpen(false)}
-              animationType="fade"
-            >
-                  <View style={styles.modal}>
-                    <View >
-                  <Text> hi </Text>
-                </View>
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    for (let i = 0; i < fakedata.length; i += 1) {
+      if (fakedata[i].id === id) {
+        setOpen(true);
+        return (
+          <Modal
+            visible={open}
+            transparent
+            onRequestClose={setOpen(false)}
+            animationType="fade"
+          >
+            <View style={styles.modal}>
+              <View>
+                <Text> hi </Text>
               </View>
-            </Modal>
-          )
-        }
+            </View>
+          </Modal>
+        );
       }
-    }) 
+    }
+  });
 }
 
 const styles = StyleSheet.create({
-    modal: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.3)',
-      },
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
 });
 
-export {ModalOpen, fakedata};
+export { ModalOpen, fakedata };
--- src/screens/CalenderScreen.jsx
+++ src/screens/CalenderScreen.jsx
@@ -1,44 +1,65 @@
 import React from 'react';
 import {
-  Text, View, StyleSheet, FlatList, TouchableOpacity
+  Text, View, StyleSheet, FlatList, TouchableOpacity,
 } from 'react-native';
 import { ModalOpen, fakedata } from '../components/CalenderDetail';
 
-
-const monthNames = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 function addZero(i) {
-  if (i < 10){
-    i = '0' + i;
+  if (i < 10) {
+    i = `0${i}`;
   }
   return i;
-};
+}
 
-const CalenderScreen = () => {
-  return(
-    <FlatList
-      data={fakedata}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => ModalOpen(1) }>
-          <View style={styles.Date_and_Schedule}>
-            <View style={styles.Date}>
-              <Text> { monthNames[ item.startTime.getMonth()] } </Text>
-              <Text style={{fontSize:16}}> { item.startTime.getDate() } </Text>
+const CalenderScreen = () => (
+  <FlatList
+    data={fakedata}
+    keyExtractor={(item) => item.id.toString()}
+    renderItem={({ item }) => (
+      <TouchableOpacity onPress={() => ModalOpen(1)}>
+        <View style={styles.Date_and_Schedule}>
+          <View style={styles.Date}>
+            <Text>
+              {' '}
+              { monthNames[item.startTime.getMonth()] }
+              {' '}
+            </Text>
+            <Text style={{ fontSize: 16 }}>
+              {' '}
+              { item.startTime.getDate() }
+              {' '}
+            </Text>
+          </View>
+          <View style={styles.Schedule}>
+            <View style={styles.Schedule_Name}>
+              <Text style={{ fontSize: 16 }}>
+                {' '}
+                { item.name }
+                {' '}
+              </Text>
             </View>
-            <View style={styles.Schedule}>
-              <View style={styles.Schedule_Name}>
-                <Text style={{fontSize:16}}> { item.name } </Text>
-              </View>
-              <View style={styles.Schedule_Time}>
-                <Text> { addZero(item.startTime.getHours()) }:{ addZero(item.startTime.getMinutes()) } - { addZero(item.endTime.getHours()) }:{ addZero(item.endTime.getMinutes()) } </Text>
-              </View>
+            <View style={styles.Schedule_Time}>
+              <Text>
+                {' '}
+                { addZero(item.startTime.getHours()) }
+                :
+                { addZero(item.startTime.getMinutes()) }
+                {' '}
+                -
+                {' '}
+                { addZero(item.endTime.getHours()) }
+                :
+                { addZero(item.endTime.getMinutes()) }
+                {' '}
+              </Text>
             </View>
-          </View> 
-        </TouchableOpacity>
-      )}
-    />
-  )
-};
+          </View>
+        </View>
+      </TouchableOpacity>
+    )}
+  />
+);
 
 const styles = StyleSheet.create({
   Date_and_Schedule: {
@@ -49,7 +70,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flex: 0.3,
-    right: '20%'
+    right: '20%',
   },
   Schedule: {
     flexDirection: 'row',
@@ -59,7 +80,7 @@ const styles = StyleSheet.create({
     right: '5%',
     borderColor: 'rgb(236, 41, 192)',
     backgroundColor: 'pink',
-    borderRadius: 15
+    borderRadius: 15,
   },
   Schedule_Name: {
     alignContent: 'flex-start',
@@ -68,7 +89,7 @@ const styles = StyleSheet.create({
   Schedule_Time: {
     justifyContent: 'flex-end',
     flexDirection: 'row',
-    flex: 1
+    flex: 1,
   },
 });
 
