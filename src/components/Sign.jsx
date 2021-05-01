119e026e6f4852414fde94f94c9b86760d83aa8f
Leo Lee <leolee0926@gmail.com>
2021-05-01 17:05:19 +0800

    do the appearance of Excute Screen

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { Text, Input } from 'react-native-elements';
-import { TouchableOpacity, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, StyleSheet } from 'react-native';
 import Spacer from './Spacer';
 import { Context } from '../context/AuthContext';
 
--- src/screens/ExecuteScreen.jsx
+++ src/screens/ExecuteScreen.jsx
@@ -4,6 +4,8 @@ import {
 } from 'react-native';
 import CountDown from 'react-native-countdown-component';
 import { AntDesign } from '@expo/vector-icons';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const fakeTask = [
   {
@@ -11,87 +13,85 @@ const fakeTask = [
     schedule: '1h30m',
   },
 ];
-function ExecuteScreen() {
-  return (
-    <View margin={150}>
-      <View style={styles.view}>
-        <Text style={styles.taskTitle}>Calculus</Text>
-      </View>
-      <View style={styles.padding}>
-        <CountDown
-          size={40}
-          until={10}
-          onFinish={() => alert('Finished')}
-          timeToShow={['H', 'M', 'S']}
-          timeLabels={{ m: null, s: null }}
-          showSeparator
-          flex={1}
-        />
-      </View>
-      <View style={styles.view1}>
-        <Text style={styles.text}>Schedule: </Text>
-        <Text style={styles.input} />
-      </View>
-      <View style={styles.view1}>
-        <Text style={styles.text}>End Time: </Text>
-        <Text style={styles.input} />
-      </View>
-      <View style={styles.view2} borderRadius={15}>
-        <AntDesign name="checkcircle" size={28} color="black" alignSelf="center" flex={1} />
-        <Text style={styles.button}>Done</Text>
-      </View>
+const ExecuteScreen = () => (
+  <View margin={100}>
+    <View style={styles.view}>
+      <Text style={styles.taskTitle}>Calculus</Text>
     </View>
-  );
-}
+    <View style={styles.padding}>
+      <Text style={styles.clock}> 00 : 54</Text>
+    </View>
+    <View style={styles.view1}>
+      <Text style={styles.text}>Schedule: </Text>
+    </View>
+    <View style={styles.view1}>
+      <Text style={styles.text}>End Time: </Text>
+    </View>
+    <View style={styles.view2} textAlign="center" marginLeft={-70} justifyContent="space-around">
+      <Button
+        icon={(
+          <Icon
+            name="check-circle"
+            size={20}
+            color="white"
+          />
+        )}
+        title="Done"
+        flex={1}
+      />
+      <Button
+        icon={(
+          <Icon
+            name="stop-circle"
+            size={20}
+            color="white"
+          />
+        )}
+        title="Stop"
+        width="20%"
+        flex={1}
+      />
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   padding: {
     flexDirection: 'row',
-    alignSelf: 'center',
-    height: 120,
-    width: 320,
-    margin: 40,
+    width: 200,
+    marginVertical: 30,
+    marginStart: -10,
   },
   view: {
     flexDirection: 'row',
-    width: 160,
-    height: 60,
-    alignSelf: 'center',
+    width: 300,
+    marginVertical: 10,
+  },
+  clock: {
+    fontSize: 50,
+    textAlign: 'center',
+    flex: 0,
   },
   view1: {
     flexDirection: 'row',
     width: 160,
     height: 60,
     alignSelf: 'center',
-    justifyContent: 'space-between',
+    marginVertical: 20,
   },
   view2: {
     flexDirection: 'row',
-    width: 120,
-    height: 50,
-    alignSelf: 'center',
-    borderColor: 'black',
-    borderWidth: 1.8,
+    width: 300,
   },
   taskTitle: {
     fontSize: 40,
-    alignSelf: 'center',
+    textAlign: 'center',
     flex: 0,
   },
   text: {
     fontSize: 25,
-    alignSelf: 'center',
+    textAlign: 'center',
     flex: 0,
-  },
-  input: {
-    borderColor: 'black',
-    borderWidth: 1.8,
-    height: 30,
-    flex: 1,
-    alignSelf: 'center',
-  },
-  button: {
-    fontSize: 30,
   },
 });
 
