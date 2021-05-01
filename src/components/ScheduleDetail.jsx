deb9e8dcbf845b46cba2a8a51c108b6184148504
shoko <michael52848@gmail.com>
2021-05-01 16:40:58 +0800

    改良ScheduleDetail 現在可以點開查看行程的詳細資訊

--- src/components/ScheduleDetail.jsx
+++ src/components/ScheduleDetail.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import {
   View, Text, Modal, StyleSheet, TouchableOpacity,
 } from 'react-native';
+import  moment  from 'moment';
+
 
 const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -15,23 +17,23 @@ function ScheduleDetail({ task }) {
     <View>
       <TouchableOpacity onPress={() => setOpen(true)}>
         <View style={styles.dateAndSchedule}>
-          <View style={styles.Date}>
+          <View style={styles.date}>
             <Text style={{ color: 'black' }}>{ getMonth(task.startTime) }</Text>
             <Text style={{ fontSize: 16 }}>
               { task.startTime.getDate() }
             </Text>
           </View>
-          <View style={styles.Schedule}>
-            <View style={styles.Schedule_Name}>
+          <View style={styles.schedule}>
+            <View style={styles.scheduleName}>
               <Text style={{ fontSize: 16 }}>
                 { task.name }
               </Text>
             </View>
-            <View style={styles.Schedule_Time}>
+            <View style={styles.scheduleTime}>
               <Text>
-                { task.startTime.toLocaleTimeString() }
+                { moment(task.startTime).format('H:mm') }
                 -
-                { task.endTime.toLocaleTimeString() }
+                { moment(task.endTime).format('H:mm') }
               </Text>
             </View>
           </View>
@@ -55,8 +57,14 @@ function TaskModal({ task, open, callback }) {
       animationType="fade"
     >
       <View style={styles.modal}>
-        <View>
-          <Text>{task.name}</Text>
+        <View style={styles.modalText}>
+          <View style={{margin: 25}}>
+            <View style={styles.taskTitle}>
+              <Text style={{ fontSize: 25 }}>{task.name}</Text>
+            </View>
+            <Text style={{ fontSize: 16 }}>Time: {moment(task.startTime).format('H:mm')} - {moment(task.endTime).format('H:mm')}</Text>
+            <Text style={{ fontSize: 16 }}>Note:</Text>
+          </View>
         </View>
       </View>
     </Modal>
@@ -65,23 +73,27 @@ function TaskModal({ task, open, callback }) {
 
 const styles = StyleSheet.create({
   modal: {
-    margin: 50,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.3)',
   },
-  Date_and_Schedule: {
+  modalText: {
+    backgroundColor: 'white',
+    height: 800,
+    width: 325,
+  },
+  dateAndSchedule: {
     flexDirection: 'row',
     marginTop: 20,
   },
-  Date: {
+  date: {
     alignItems: 'center',
     justifyContent: 'center',
     flex: 0.3,
     right: '20%',
   },
-  Schedule: {
+  schedule: {
     flexDirection: 'row',
     alignItems: 'center',
     borderWidth: 1,
@@ -91,15 +103,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'pink',
     borderRadius: 15,
   },
-  Schedule_Name: {
+  scheduleName: {
     alignContent: 'flex-start',
     flexDirection: 'row',
+    left: 10
   },
-  Schedule_Time: {
+  scheduleTime: {
     justifyContent: 'flex-end',
     flexDirection: 'row',
     flex: 1,
+    right: 10
   },
+  taskTitle: {
+    alignItems: 'center',
+    bottom: 10
+  }
 });
 
 export default ScheduleDetail;
